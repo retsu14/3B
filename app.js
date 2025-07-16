106a827108b04d1ba7f2b4dd8aaf4062a7910928
retsu14 <tajanlangitelieser19@gmail.com>
2025-07-16 20:01:06 +0800

    chore: object

---
+++ app.js
@@ -0,0 +1,16 @@
+const contacts = [
+  {
+    profile: "picture.jpeg",
+    name: "Mark Jamandre",
+    status: "active",
+  },
+  {
+    profile: "picture.jpeg",
+    name: "sge okay",
+    status: "offline",
+  },
+];
+
+contacts.forEach((contact) => {
+  console.log("username", contact.name);
+});
